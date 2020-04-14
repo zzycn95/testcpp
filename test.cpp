cc4831458ee1e79587c1e9467a3f479b1e8a650d
zzycn95 <zhang_zongyao@126.com>
2020-04-14 21:08:55 +0800

    i++,++i

--- test.cpp
+++ test.cpp
@@ -2,11 +2,14 @@
 #include<stdio.h>
 using namespace std;
 int main(int args,char* argv[]){
-    cout<<"---------------zhangzongyao-----------------------"<<endl;
+    cout<<"--------------------------------------"<<endl;
     for (int i = 0; i < args; i++)
     {
         cout<<argv[i]<<endl;
     }
+    int i=0;
+    int j=i++;
+    cout<<j<<" "<<i<<endl;
     cout<<"--------------------------------------"<<endl;
     return 0;
 }
