979164b5fde0bd129e0696cab638c79975246998
zzycn95 <zhang_zongyao@126.com>
2020-04-14 15:40:12 +0800

    add=================================

--- test.cpp
+++ test.cpp
@@ -2,9 +2,11 @@
 #include<stdio.h>
 using namespace std;
 int main(int args,char* argv[]){
+    cout<<"--------------------------------------"<<endl;
     for (int i = 0; i < args; i++)
     {
         cout<<argv[i]<<endl;
     }
+    cout<<"--------------------------------------"<<endl;
     return 0;
 }
